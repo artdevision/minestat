6535c91eb2a582318f2c0511083b085d12c47bfb
Myslin Andrey <artdevision@gmail.com>
2019-04-08 16:43:16 +0500

    Optimize indexes

--- config/app.php
+++ config/app.php
@@ -174,7 +174,7 @@ return [
         App\Providers\AuthServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
-        App\Providers\HorizonServiceProvider::class,
+        //App\Providers\HorizonServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
         Collective\Html\HtmlServiceProvider::class,
